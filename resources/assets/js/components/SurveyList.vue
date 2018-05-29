2f7924efa7ecba9786288ce167f5f1dab90c275e
Aidy Makhataev <makataev.7@gmail.com>
2018-05-29 13:03:55 +0600

    add validation on text field, redesigned toolbar

--- resources/assets/js/components/SurveyList.vue
+++ resources/assets/js/components/SurveyList.vue
@@ -1,73 +1,77 @@
 <template>
     <div>
-        <v-card>
-            <v-card-title>
-                List of all available surveys
-                <v-spacer></v-spacer>
-                <v-dialog v-model="dialog" max-width="500px" content-class="remove-overflow">
-                    <v-btn slot="activator" color="primary" dark class="mb-2">New Survey</v-btn>
-                    <v-card>
-                        <v-card-title>
-                            <span class="headline">{{ formTitle }}</span>
-                        </v-card-title>
-                        <v-card-text>
-                            <v-container grid-list-md>
-                                <v-layout wrap>
-                                    <v-flex xs12 sm6 md4>
-                                        <v-text-field v-model="editedItem.name" label="Survey name"></v-text-field>
-                                    </v-flex>
-                                </v-layout>
-                            </v-container>
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn color="blue darken-1" flat @click.native="onCloseModal">Cancel</v-btn>
-                            <v-btn color="blue darken-1" flat @click.native="onSaveModal(editedItem.name)">Save</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
-            </v-card-title>
-            <v-data-table
-                    :headers="headers"
-                    :items="surveys"
-                    :loading="loading"
-                    hide-actions
-                    class="elevation-1"
-            >
-                <template slot="items" slot-scope="props">
-                    <td class="text-sm-left">{{ props.item.id }}</td>
-                    <td class="text-sm-left">{{ props.item.name }}</td>
-                    <td class="text-sm-left">{{ props.item.created_at}}</td>
-                    <td class="justify-center layout px-0">
-                        <v-btn icon class="mx-0" @click="editItem(props.item)">
-                            <v-icon color="teal">edit</v-icon>
-                        </v-btn>
-                        <v-btn icon class="mx-0" @click="deleteItem(props.item)">
-                            <v-icon color="pink">delete</v-icon>
-                        </v-btn>
-                    </td>
-                </template>
-                <template slot="no-data">
-                    <v-btn color="primary" @click="initialize">Reset</v-btn>
-                </template>
-            </v-data-table>
-            <div class="text-xs-center pt-2">
-                <v-pagination v-model="page" :length="pageLength" :total-visible="7"></v-pagination>
-            </div>
-            <v-snackbar
+        <v-toolbar>
+            <v-toolbar-title>List of all available surveys</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-dialog flat v-model="dialog" max-width="500px" content-class="remove-overflow">
+                <v-btn slot="activator" color="primary" dark class="mb-2">New Survey</v-btn>
+                <v-card>
+                    <v-card-title>
+                        <span class="headline">{{ formTitle }}</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container grid-list-md>
+                            <v-layout wrap>
+                                <v-flex xs12 sm6 md4>
+                                    <v-text-field
+                                            v-model="editedItem.name"
+                                            label="Survey name"
+                                            :rules="[
+                                            () => !!editedItem.name || 'The field name is required',
+                                            () => !!editedItem.name && editedItem.name.length >= 3 || 'Name must contain at least 3 character!',
+                                            ]"
+                                    >
+
+                                    </v-text-field>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" flat @click.native="onCloseModal">Cancel</v-btn>
+                        <v-btn color="blue darken-1" flat @click.native="onSaveModal(editedItem.name)">Save</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-toolbar>
+        <v-data-table
+                :headers="headers"
+                :items="surveys"
+                :loading="loading"
+                hide-actions
+                class="elevation-1"
+        >
+            <template slot="items" slot-scope="props">
+                <td class="text-sm-left">{{ props.item.id }}</td>
+                <td class="text-sm-left">{{ props.item.name }}</td>
+                <td class="text-sm-left">{{ props.item.created_at}}</td>
+                <td class="justify-center layout px-0">
+                    <v-btn icon class="mx-0" @click="editItem(props.item)">
+                        <v-icon color="teal">edit</v-icon>
+                    </v-btn>
+                    <v-btn icon class="mx-0" @click="deleteItem(props.item)">
+                        <v-icon color="pink">delete</v-icon>
+                    </v-btn>
+                </td>
+            </template>
+            <template slot="no-data">
+                <v-btn color="primary" @click="initialize">Reset</v-btn>
+            </template>
+        </v-data-table>
+        <div class="text-xs-center pt-2">
+            <v-pagination v-model="page" :length="pageLength" :total-visible="7"></v-pagination>
+        </div>
+        <v-snackbar
                 :timeout="6000"
                 bottom
                 right
                 multi-line
                 v-model="snackbar"
-            >
-                {{ snackbarMsg }}
-                <v-btn flat color="blue" @click.native="snackbar = false"></v-btn>
-            </v-snackbar>
-
-        </v-card>
-
-
+        >
+            {{ snackbarMsg }}
+            <v-btn flat color="blue" @click.native="snackbar = false"></v-btn>
+        </v-snackbar>
     </div>
 </template>
 
